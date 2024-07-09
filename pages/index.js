f814b21ca40128cdd5f6fe13f943d76788865413
masud rana <masudfcs1@gmail.com>
2024-07-09 17:35:45 +0600

    full stack file manger

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,14 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-   <div className=" font-bold uppercase underline">
-  hello
-   </div>
+    <div className=" font-bold uppercase underline">
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
+    </div>
   );
 }
