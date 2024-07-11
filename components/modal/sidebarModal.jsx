8314c3cf61eaef810e97370e7e4a87ad55da3e07
masud rana <masudfcs1@gmail.com>
2024-07-11 23:52:16 +0600

    update

--- components/modal/sidebarModal.jsx
+++ components/modal/sidebarModal.jsx
@@ -3,11 +3,11 @@ import { useState } from 'react';
 
 const SidebarModal = () => {
     const [inputField, setinputField] = useState()
-    
 
-    const onCreate= async()=>{
+
+    const onCreate = async () => {
         console.log(inputField);
-        
+
     }
 
     return (
@@ -17,19 +17,18 @@ const SidebarModal = () => {
                 <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                 <div className=' w-full flex flex-col justify-center items-center gap-2'>
                     <Image
-                    src='/folder.png'
-                    width={50}
-                    height={50}
+                        src='/folder.png'
+                        width={50}
+                        height={50}
                     />
                     <input
-                     type="text"
-                     placeholder='Folder Name '
-                     className=' w-full outline-none p-2 bg-white border-[1px] rounded-sm'
-                     onChange={(e)=>setinputField(e.target.value)}
-                     />
+                        type="text"
+                        placeholder='Folder Name '
+                        className=' w-full outline-none p-2 bg-white border-[1px] rounded-sm'
+                        onChange={(e) => setinputField(e.target.value)}
+                    />
 
-                     
-                     <button onClick={()=>onCreate()} type='submit' className=' p-2 bg-blue-500 text-white mt-3 w-full rounded-sm hover:bg-blue-600 '>Create</button>
+                    <button onClick={() => onCreate()} type='submit' className=' p-2 bg-blue-500 text-white mt-3 w-full rounded-sm hover:bg-blue-600 '>Create</button>
                 </div>
             </form>
         </div>
--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -22,7 +22,7 @@ const Sidebar = () => {
                 </Link>
             </div>
 
-            <button  className=' w-[150px] flex mt-10 gap-2 text-[15px] items-center text-white bg-blue-500 p-2 rounded-sm hover:scale-105 '> Add New File
+            <button className=' w-[150px] flex mt-10 gap-2 text-[15px] items-center text-white bg-blue-500 p-2 rounded-sm hover:scale-105 '> Add New File
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                     <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z" clipRule="evenodd" />
                 </svg>
