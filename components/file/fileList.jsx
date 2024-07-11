d5ccd4779ba19ad232cd50880d6abd46065a581a
masud rana <masudfcs1@gmail.com>
2024-07-11 23:57:51 +0600

    update

--- components/file/fileList.jsx
+++ components/file/fileList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import FileItem from './fileItem'
 
 const FileList = () => {
-    const filelist =[
+    const filelist = [
         {
             id: 1,
             name: "DSA",
@@ -32,23 +32,23 @@ const FileList = () => {
             modifiedAt: "Jan 21,2023"
         },
     ]
-  return (
-    <div className=' bg-white mt-5 p-5 rounded-sm '>
-        <h2 className=' font-bold text-[18px] '>Recent files</h2>
-        <div className=' grid grid-cols-1 md:grid-cols-2 text-[13px] font-semibold  border-b-[1px] mt-3 pb-1  border-gray-300 text-gray-400'>
-            <h2>Name</h2>
-            <div className=' grid grid-cols-3'>
-                <h2> Modified</h2>
-                <h2> Size</h2>
+    return (
+        <div className=' bg-white mt-5 p-5 rounded-sm '>
+            <h2 className=' font-bold text-[18px] '>Recent files</h2>
+            <div className=' grid grid-cols-1 md:grid-cols-2 text-[13px] font-semibold  border-b-[1px] mt-3 pb-1  border-gray-300 text-gray-400'>
+                <h2>Name</h2>
+                <div className=' grid grid-cols-3'>
+                    <h2> Modified</h2>
+                    <h2> Size</h2>
+                </div>
             </div>
+
+            {filelist && filelist.map((item, index) => (
+                <FileItem file={item} key={index} />
+            ))}
+
         </div>
-
-        { filelist&&filelist.map((item,index)=>(
-            <FileItem file={item} key={index} />
-        )) }
-
-    </div>
-  )
+    )
 }
 
 export default FileList
