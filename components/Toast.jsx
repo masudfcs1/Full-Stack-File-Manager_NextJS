6ab69613f0ef36d5bfeb728bc5106a2532d02ac7
masud rana <masudfcs1@gmail.com>
2024-07-14 01:27:08 +0600

    final toast

--- components/Toast.jsx
+++ components/Toast.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { ShowToastContext } from './ShowToastContext'
 
-const Toast = ({message}) => {
+const Toast = ({ message }) => {
+    const { showToastMsg, setShowToastMsg } = useContext(ShowToastContext)
+
+    useEffect(() => {
+        setInterval(() => {
+            setShowToastMsg(null)
+        }, 3000)
+    }, [showToastMsg])
     return (
-        <div className="toast toast-end">
+        <div className="toast toast-first">
             <div className="alert alert-success">
                 <span>{message} </span>
             </div>
